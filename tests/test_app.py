e4706b1be02d6e286856c2b179279f0dd8ec64a7
Sreedhar Busanelli <sridhar_busanelli@yahoo.com>
2022-05-01 22:04:21 -0500

    Created mapping for US location to test for it

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,5 +1,18 @@
-from app import index
 
-def test_index():
-    assert index() == "Github Workflow Example"
+import sys
+import unittest
+from common import us_state_mapping
+import requests
 
+class TestAppMethods(unittest.TestCase):
+  def test_index(self):
+    self.assertEqual('foo'.upper(), 'FOO')
+  def test_us_location(self):
+    r = requests.get('https://ipinfo.io')
+    rd = r.json()
+    state = rd.get('region')
+    tlc = us_state_mapping.us_state_to_tlc[state]
+    self.assertEqual(state , us_state_mapping.tlc_to_us_state[tlc])
+  
+if __name__ == '__main__':
+    unittest.main()
